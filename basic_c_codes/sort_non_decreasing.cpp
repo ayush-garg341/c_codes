ad40bdafc057df00ed5c44535ed6ab5df6232d88
garg_ayush341 <gargayush341@gmail.com>
2020-09-24 09:20:46 +0530

    sort non-decreasing using two pointers front, end.

--- basic_c_codes/sort_non_decreasing.cpp
+++ basic_c_codes/sort_non_decreasing.cpp
@@ -5,69 +5,29 @@ using namespace std;
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& A) {
-        vector<int>Negative_numbers_square;
-        vector<int>Positive_numbers_square;
-        vector<int>Merge_numbers;
-        unsigned int array_length{static_cast<unsigned int>(A.size())};
-        for(int i{0}; i<array_length; i++){
-            if(A[i]<0){
-	      //cout<<A[i]<<"  "<<A[i]*A[i]<<"\n";
-	      Negative_numbers_square.insert(Negative_numbers_square.begin(), A[i]*A[i]);
+        vector<int>Merge_numbers(A.size());
+        int leftPointer{0};
+        int rightPointer{static_cast<int>(A.size()-1)};
+        int index{rightPointer};
+        while(leftPointer<=rightPointer){
+            int leftSquare = A[leftPointer]*A[leftPointer];
+            int rightSquare = A[rightPointer]*A[rightPointer];
+            if(leftSquare>rightSquare){
+                Merge_numbers[index]=leftSquare;
+                leftPointer++;
+                index--;
             }
             else{
-	      //cout<<A[i]<<"  "<<A[i]*A[i]<<"\n";
-                Positive_numbers_square.push_back(A[i]*A[i]);
+                Merge_numbers[index]=rightSquare;
+                rightPointer--;
+                index--;
             }
         }
         
-        if(Positive_numbers_square.size()==0){
-            return Negative_numbers_square;
-        }
-        
-        if(Negative_numbers_square.size()==0){
-            return Positive_numbers_square;
-        }
-        
-        unsigned int j{0};
-        unsigned int k{0};
-	//cout<<"positive number size "<< Positive_numbers_square.size() << "\n";
-	//cout<<"negative number size "<< Negative_numbers_square.size() << "\n";
-        while(j<Negative_numbers_square.size() && k<Positive_numbers_square.size()){
-            if(Negative_numbers_square[j] < Positive_numbers_square[k]){
-                Merge_numbers.push_back(Negative_numbers_square[j]);
-                j++;
-            }
-            else{
-                Merge_numbers.push_back(Positive_numbers_square[k]);
-                k++;
-            }
-        }
-
-	//cout<<"j "<<j<<"\n";
-	//cout<<"k "<<k<<"\n";
-	
-        if(k==Positive_numbers_square.size()){
-            while(j<Negative_numbers_square.size()){
-                Merge_numbers.push_back(Negative_numbers_square[j]);
-                j++;
-            }
-        }
-        else if(j==Negative_numbers_square.size()){
-            while(k<Positive_numbers_square.size()){
-                Merge_numbers.push_back(Positive_numbers_square[k]);
-                k++;
-            }
-        }
-
-	for(int l{0} ; l< Merge_numbers.size();l++){
-	  cout<<Merge_numbers[l]<<"\n";
-	}
-	
         return Merge_numbers;
-        
     }
+        
 };
-
 
 int main(){
   Solution soln;
