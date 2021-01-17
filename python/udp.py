5dfa267457c4744ce7552ebb925c327a97f61953
Ayush Garg <ayush.garg@tripock.com>
2021-01-17 10:24:51 +0530

    get messges from expected hosts only by taking host address...

--- python/udp.py
+++ python/udp.py
@@ -16,16 +16,22 @@ def server(port):
         s.sendto(data, clientAddress)
 
 def client(port):
-    host = '127.0.0.1'
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(host, port)
-    message = input('Input lowercase sentence:' )
-    data = message.encode('ascii')
-    s.send(data)
-    print('The OS assigned the address {} to me'.format(s.getsockname()))
-    data, address = s.recv(MAX_SIZE_BYTES) 
-    text = data.decode('ascii')
-    print('The server {} replied with {!r}'.format(address, text))
+    hosts = []
+    while True:
+        host = input('Input host address:' )
+        hosts.append((host,port))
+        message = input('Input message to send to server:' )
+        data = message.encode('ascii')
+        s.sendto(data, (host, port))
+        print('The OS assigned the address {} to me'.format(s.getsockname()))
+        data, address = s.recvfrom(MAX_SIZE_BYTES)
+        text = data.decode('ascii')
+        if(address in hosts):
+            print('The server {} replied with {!r}'.format(address, text))
+            hosts.remove(address)
+        else:
+            print('message {!r} from unexpected host {}!'.format(text, address))
 
 if __name__ == '__main__':
     funcs = {'client': client, 'server': server}
