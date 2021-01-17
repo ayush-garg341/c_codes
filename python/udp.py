af1772424929d8303ae8b77067b0c22e15504685
Ayush Garg <ayush.garg@tripock.com>
2021-01-17 10:00:39 +0530

    improved udp, where client will recv msgs from connected server not from any server, that was happening previously.

--- python/udp.py
+++ python/udp.py
@@ -16,12 +16,14 @@ def server(port):
         s.sendto(data, clientAddress)
 
 def client(port):
+    host = '127.0.0.1'
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(host, port)
     message = input('Input lowercase sentence:' )
     data = message.encode('ascii')
-    s.sendto(data, ('127.0.0.1', port))
+    s.send(data)
     print('The OS assigned the address {} to me'.format(s.getsockname()))
-    data, address = s.recvfrom(MAX_SIZE_BYTES) 
+    data, address = s.recv(MAX_SIZE_BYTES) 
     text = data.decode('ascii')
     print('The server {} replied with {!r}'.format(address, text))
 
