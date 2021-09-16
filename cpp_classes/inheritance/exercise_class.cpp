1c8b4a322786efe9db467cf46608221f77541e9a
Ayush Garg <gargayush341@gmail.com>
2021-09-16 15:06:40 +0530

    changed the exercise file for calculator classs...

--- cpp_classes/inheritance/exercise_class.cpp
+++ cpp_classes/inheritance/exercise_class.cpp
@@ -1,87 +1,87 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 
+/*
+  Create 2 classes:
+    1. SimpleCalculator :- take input of two numbers using a utility function and performs +, -. *, / and displays the result
+      using another function.
+
+    2. ScientificCalculator :- Take input of two numbers using a utitlity function and performs four scientific ops. of your choice
+      and displays the result using another function.
+
+    
+    3. Create Another class HybridCalculator and inherit it using these 2 classes
+
+      1. What type of inheritance you are using ? -> Multiple inheritance 
+      2. What mode of inheritance you are using ? -> public
+      3. How is code re-usability implemented ?
+      4. Create an object of HybridCalculator and displays result of simple and scientific calculator.
+
+*/
+
+
 class SimpleCalculator{
-protected:
-  int num1;
-  int num2;
-  int result;
+  int a;
+  int b;
   
 public:
-  void set_two_nums(int a, int b){
-    num1 = a;
-    num2 = b;
+  void get_data_simple(){
+    cout << "Enter the value of a " << endl;
+    cin >> a;
+    cout << "Enter the value of b" << endl;
+    cin >> b;
   }
 
-  void add_two_nums(){
-    result = num1 + num2;
-  };
-
-  void sub_two_nums(){
-    result = num1 - num2;
-  }
-
-  void multiply_two_nums(){
-    result = num1 * num2;
-  }
-
-  void divide_two_nums(){
-    if(num2 == 0){
-      cout << "Invalid: division by 0 can not be performed" << endl;
-      return ;
-    }
-    result = num1 / num2;
-  }
-
-
-  void display(){
-    cout << "The result of the calculation is " << result << endl;
+  void performOpsSimple(){
+    cout << "The value of a+b is " << a+b << endl;
+    cout << "The value of a-b is " << a-b << endl;
+    cout << "The value of a*b is " << a*b << endl;
+    cout << "The value of a/b is " << a/b << endl;
   }
   
 };
 
 
-class ScientificCalculator: protected SimpleCalculator{
-
-  public:
-  void add_two_nums_scientifically(){
-    cout << "syntific add" << endl;
-    result = num1 + num2;
-  };
-
-  void sub_two_nums_scientifically(){
-    cout << "syntific subtract" << endl;
-    result = num1 - num2;
+class ScientificCalculator {
+  int a;
+  int b;
+  
+public:
+  void get_data_scientific(){
+    cout << "Enter the value of a " << endl;
+    cin >> a;
+    cout << "Enter the value of b" << endl;
+    cin >> b;
   }
 
-  void multiply_two_nums_scientifically(){
-    cout << "syntific multiply" << endl;
-    result = num1 * num2;
-  }
-
-  void divide_two_nums_scientifically(){
-    cout << "syntific divide" << endl;
-    if(num2 == 0){
-      cout << "Invalid: division by 0 can not be performed" << endl;
-      return ;
-    }
-    result = num1 / num2;
+  void performOpsScientific(){
+    cout << "The value of cos(a) = " << cos(a) << endl;
+    cout << "The value of sin(a) = " << sin(a) << endl;
+    cout << "The value of exp(a) = " << exp(a) << endl;
+    cout << "The value of tan(a) = " << tan(a) << endl;
   }
   
 };
 
 
 class HybridCalculator: public SimpleCalculator, public ScientificCalculator{
-public:
-  void display(){
-    cout << "Inside Hybrid calculator" << endl;
-  };
-  
+
 };
 
 
 int main(){
-  
+  // SimpleCalculator calc;
+  // calc.get_data_simple();
+  // calc.performOpsSimple();
+
+  HybridCalculator hcalc;
+  hcalc.get_data_simple();
+  hcalc.performOpsSimple();
+
+  hcalc.get_data_scientific();
+  hcalc.performOpsScientific();
+
   return 0;
 }
