e735feb94aae442db51c9bf4a62127cb3165c561
Ayush Garg <gargayush341@gmail.com>
2025-04-14 16:55:35 +0530

    byte value from int, float

---
+++ advance_c/byte_value.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+typedef unsigned char* byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len) {
+    int i;
+    for(i = 0; i < len; i++)
+    {
+        printf(" %0.2x", start[i]);
+    }
+
+    printf("\n");
+}
+
+void show_int(int x) {
+    show_bytes((byte_pointer) &x, sizeof(x));
+}
+
+void show_float(float x) {
+    show_bytes((byte_pointer) &x, sizeof(x));
+}
+
+void show_pointer(void *x) {
+    show_bytes((byte_pointer) &x, sizeof(void *));
+}
+
+int main() {
+    int ival = 1024;
+    float fval = (float) ival;
+    int *pval = &ival;
+    show_int(ival);
+    show_float(fval);
+    show_pointer(pval);
+}
